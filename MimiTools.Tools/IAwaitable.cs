590aabb8fa67e3e57c22ebc91e48aed7f7b7ea5b
jaypaw549 <jaypaw549@gmail.com>
2019-12-19 18:34:41 -0700

    Add awaitable interfaces

---
+++ MimiTools.Tools/IAwaitable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MimiTools.Tools
+{
+    public interface IAwaitable
+    {
+        IAwaiter GetAwaiter();
+    }
+
+    public interface IAwaitable<TResult>
+    {
+        IAwaiter<TResult> GetAwaiter();
+    }
+
+    public interface ICustomAwaitable<TAwaiter> where TAwaiter : IAwaiter
+    {
+        TAwaiter GetAwaiter();
+    }
+
+    public interface ICustomAwaitable<TAwaiter, TResult> where TAwaiter : IAwaiter<TResult>
+    {
+        TAwaiter GetAwaiter();
+    }
+
+    public interface IAwaiter : INotifyCompletion
+    {
+        bool IsCompleted { get; }
+
+        void GetResult();
+    }
+
+    public interface IAwaiter<TResult> : INotifyCompletion
+    {
+        bool IsCompleted { get; }
+
+        TResult GetResult();
+    }
+}
