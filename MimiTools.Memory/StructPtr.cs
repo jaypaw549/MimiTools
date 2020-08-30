d806bd31f77af704e985a6987ab17046dfe849de
jaypaw549 <jaypaw549@gmail.com>
2020-08-29 21:32:55 -0600

    Add more unmanaged stuff

---
+++ MimiTools.Memory/StructPtr.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MimiTools.Memory
+{
+    public readonly struct StructPtr<T> where T : unmanaged
+    {
+        public IntPtr Pointer { get; }
+
+        public unsafe ref T Reference => ref *(T*)Pointer;
+
+        public T Value { get => Reference; set => Reference = value; }
+
+        public unsafe StructPtr(ref T target)
+        {
+            fixed (T* ptr = &target)
+                Pointer = new IntPtr(ptr);
+        }
+    }
+}
